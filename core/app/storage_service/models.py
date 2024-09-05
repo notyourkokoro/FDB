a994062d35bdd871d18fff1eb845c3e1561dec04
Sonel <dmitriew.vsevolod@yandex.ru>
2024-09-05 22:32:03 +0800

    Добавление моделей для сервиса работы с файлами

---
+++ core/app/storage_service/models.py
@@ -0,0 +1,68 @@
+from enum import Enum
+
+from datetime import datetime
+from typing import TYPE_CHECKING
+
+from sqlalchemy import ForeignKey, func
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.database import Base
+
+
+if TYPE_CHECKING:
+    from app.auth_service.models import User
+
+
+class FileTypeEnum(Enum):
+    csv = 1
+    xlsx = 2
+    xls = 3
+
+
+class FileType(Base):
+    __tablename__ = "file_types"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)
+
+
+class StorageFile(Base):
+    __tablename__ = "storage_files"
+
+    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
+    creator_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    filename: Mapped[str] = mapped_column(unique=True)
+    path: Mapped[str]
+    size: Mapped[int]
+    type_id: Mapped[int] = mapped_column(ForeignKey("file_types.id"))
+    upload_date: Mapped[datetime] = mapped_column(
+        default=datetime.now, server_default=func.now()
+    )
+    update_date: Mapped[datetime] = mapped_column(
+        default=datetime.now,
+        onupdate=datetime.now,
+        server_default=func.now(),
+        server_onupdate=func.now(),
+    )
+
+    users: Mapped[list["User"]] = relationship(
+        secondary="users_files", back_populates="files", passive_deletes=True
+    )
+
+    def __repr__(self):
+        return "{name}(id={id}, name={filename})".format(
+            name=self.__class__.__name__,
+            id=self.id,
+            filename=self.filename,
+        )
+
+
+class UserFile(Base):
+    __tablename__ = "users_files"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
+    file_id: Mapped[int] = mapped_column(
+        ForeignKey("storage_files.id", ondelete="CASCADE")
+    )
