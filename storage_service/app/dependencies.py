734fe4fd0d74fd7ac631b75c657df9b030e17d4d
Sonel <dmitriew.vsevolod@yandex.ru>
2024-09-13 21:15:56 +0800

    Переход с requests на httpx

--- storage_service/app/dependencies.py
+++ storage_service/app/dependencies.py
@@ -1,4 +1,4 @@
-import requests
+import httpx
 
 from fastapi import Security
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
@@ -12,9 +12,9 @@ async def get_current_user_uuid(
     credentials: HTTPAuthorizationCredentials = Security(security),
 ) -> str | None:
     token = credentials.credentials
-    response = requests.post(
-        f"{settings.auth_url}/auth/jwt/decode",
-        json={"token": token},
-    )
-
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            f"{settings.auth_url}/auth/jwt/decode",
+            json={"token": token},
+        )
     return response.json().get("uuid")
