9ff18b752a2cd9823bbe0ea6cd8bee7c2c40e6cd
Sonel <dmitriew.vsevolod@yandex.ru>
2024-11-09 10:50:16 +0800

    Добавление админ-панели

--- core/app/main.py
+++ core/app/main.py
@@ -1,4 +1,13 @@
 from fastapi import FastAPI
+from sqladmin import Admin
 
+from app.database import async_db
+
+from app.views import UserView, FileTypeView, StorageFileView
 
 app = FastAPI()
+admin = Admin(app, async_db.engine)
+
+
+for model in [UserView, FileTypeView, StorageFileView]:
+    admin.add_view(model)
---
+++ core/app/views.py
@@ -0,0 +1,25 @@
+from sqladmin import ModelView
+
+from app.storage_service.models import (  # noqa F401 - необходимо для корректной работы
+    UserFile,
+    FileType,
+    StorageFile,
+)
+from app.auth_service.models import User
+
+
+class UserView(ModelView, model=User):
+    column_list = [User.id, User.email, User.is_active, User.is_superuser]
+
+
+class FileTypeView(ModelView, model=FileType):
+    column_list = [FileType.id, FileType.name]
+
+
+class StorageFileView(ModelView, model=StorageFile):
+    column_list = [
+        StorageFile.id,
+        StorageFile.filename,
+        StorageFile.type_id,
+        StorageFile.size,
+    ]
