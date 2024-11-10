aac96f12a3742519b2bdd0647ebb069c08f787b5
Sonel <dmitriew.vsevolod@yandex.ru>
2024-11-10 16:25:21 +0800

    Добавление возможности работать с файлами из групп, в которых состоит пользователь

--- storage_service/app/permissions.py
+++ storage_service/app/permissions.py
@@ -1,6 +1,9 @@
+from uuid import UUID
+
+from sqlalchemy import select, and_
+from app.models import UserGroup, UserFile, GroupFile
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.models import User, StorageFile
 from app.repository import (
     select_file,
     select_user,
@@ -9,28 +12,46 @@ from app.repository import (
 
 class StorageFilePermission:
     @staticmethod
-    def check_user_access(user: User, storage_file: StorageFile) -> bool:
-        if user.is_superuser:
-            return True
-        if user.id == storage_file.creator_id:
-            return True
-        if storage_file in user.files:
-            return True
-        return False
+    async def has_user_access_to_file(
+        user_id: UUID, file_id: int, session: AsyncSession
+    ) -> bool:
+        # Подзапрос для получения групп пользователя
+        user_groups_subquery = (
+            select(UserGroup.group_id).where(UserGroup.user_id == user_id).subquery()
+        )
+
+        # Проверка на наличие файла в личных файлах пользователя (User.files)
+        user_files_query = select(UserFile).where(
+            and_(UserFile.user_id == user_id, UserFile.file_id == file_id)
+        )
+        user_file_exists = await session.execute(user_files_query)
+        user_file_exists = user_file_exists.scalar() is not None
+
+        # Проверка на наличие файла в группах пользователя
+        group_files_query = select(GroupFile).where(
+            and_(
+                GroupFile.file_id == file_id,
+                GroupFile.group_id.in_(user_groups_subquery),
+            )
+        )
+        group_file_exists = await session.execute(group_files_query)
+        group_file_exists = group_file_exists.scalar() is not None
+
+        # Если файл есть в личных файлах или в группах, возвращаем True
+        return user_file_exists or group_file_exists
 
     @staticmethod
     async def check_user_access_with_data(
-        user_id: str, storage_file_id: int, session: AsyncSession
+        user_id: str, file_id: int, session: AsyncSession
     ) -> dict:
-        user = await select_user(user_id=user_id, session=session)
-        storage_file = await select_file(file_id=storage_file_id, session=session)
-
         if (
-            StorageFilePermission.check_user_access(
-                user=user, storage_file=storage_file
+            await StorageFilePermission.has_user_access_to_file(
+                user_id=UUID(user_id), file_id=file_id, session=session
             )
             is False
         ):
             return {"access": False}
 
+        user = await select_user(user_id=user_id, session=session)
+        storage_file = await select_file(file_id=file_id, session=session)
         return {"user": user, "storage_file": storage_file, "access": True}
